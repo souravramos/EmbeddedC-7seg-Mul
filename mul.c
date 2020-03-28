9952ab78d4ebe13d8444ed5ab2ad289b92ae0880
souravramos <souravramos04@gmail.com>
2020-03-28 17:39:37 +0530

    Embedded C project

---
+++ mul.c
@@ -0,0 +1,20 @@
+#include <REG51F.H>
+
+void main(void)
+{
+
+}
+//-----------------------------------------------
+void display(display)
+{
+	
+}
+//-----------------------------------------------
+void delay(unsigned int a)
+{
+	unsigned int i,j;
+	for(i=0;i<a;i++)
+		for(j=0;j<120;j++);
+}
+//-----------------------------------------------
+// END OF PROGRAM
